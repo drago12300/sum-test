f1919748ff34c65fa812d4e5c04fa6a4fc5ee56b
drago12300 <snotragink@gmail.com>
2016-04-16 22:14:01 -0500

    Added files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// Test2.cpp - adds 1 + 1 and displays 2
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+  int sum; // use this variable to store the sum of two numbers
+
+  sum = 1 + 1;
+
+  cout << "The sum of 1 and 1 is " << sum << endl;
+
+}
